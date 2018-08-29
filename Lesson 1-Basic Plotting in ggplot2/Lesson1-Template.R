0caac2357cbcb8d7a685b92a963b880f66684e6d
darrenpackt <34567615+darrenpackt@users.noreply.github.com>
2018-08-29 15:13:15 +0530

    Create Lesson1-Template.R

---
+++ Lesson 1-Basic Plotting in ggplot2/Lesson1-Template.R
@@ -0,0 +1,18 @@
+'
+Applied Data Visualization with ggplot2 - Lesson 1
+Description: Students template code. This has the libraries 
+and packages pre-installed. Students need to use this to write
+their codes in.
+Instructions:
+Prior to using this code install the packages: 
+install.packages("ggplot2")
+install.packages("tibble")
+install.packages("dplyr")
+install.packages("Lock5Data")
+'
+
+#Load Libraries
+require("ggplot2")
+require("tibble")
+require("dplyr")
+require("Lock5Data")
